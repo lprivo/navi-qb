f1c29c279415441c385356cf8ff18a99b94515c2
LPrivo <privinbox@gmail.com>
2020-09-07 00:00:29 +0100

    refact-1

--- navi-qb.js
+++ navi-qb.js
@@ -1,7 +1,16 @@
 
+let grid;
+let qb = document.getElementById("qb");
+let qbObj = { icon: "url('templates/images/nqb-icon.png')", dimension: 0, position: 0, direction: 0 };
+
+const getDimension = () => {
+    const dimension = grid.clientHeight / 5;
+    return dimension;
+}
+
 const gridSetup = (grid) => {
 
-    for (i = 1; i <= 20; i++) {
+    for (i = 1; i <= 30; i++) {
         const newSquare = document.createElement("div");
 
         newSquare.setAttribute("id", `sq${i}`);
@@ -10,39 +19,53 @@ const gridSetup = (grid) => {
         newSquare.childNodes[0].innerHTML = i;
         grid.appendChild(newSquare);
     };
-};
+}
 
-const qbSetup = (grid) => {
-    const qb = document.createElement("div");
-    let dimension = 160;
+const qbSetup = (grid, dimension) => {
+    // const qb = document.getElementById("qb");
 
-    qb.setAttribute("id", "qb");
-    qb.setAttribute("style", `width: ${dimension}px; height: ${dimension}px; background-image:url('templates/images/nqb-icon.png')`);
+    // qb.setAttribute("id", "qb");
+    qb.setAttribute("style", `width: ${dimension}px; height: ${dimension}px; background-image:${qbObj.icon}`);
     grid.appendChild(qb);
-};
+}
+
+const qbResize = () => {
+    const dimension = getDimension();
+    qb.setAttribute("style", `width: ${dimension}px; height: ${dimension}px; background-image:${qbObj.icon}`);
+    console.log('qbObj: ', qbObj);
+}
+
+const turn90 = (direction) => {
+
+}
 
 const animQb = () => {
-    const qb = document.getElementById("qb");
-    let pos = 0;
-    let rotateAngle = 90;
-    let dimension = 160;
+    // const qb = document.getElementById("qb");
+    const dimension = getDimension();
+    // let pos = 0;
+    // let rotateAngle = 90;
+
     const forward = () => {
-        if (pos == 160) {
-            clearInterval(id);
+        if (qbObj.position === dimension) {
+            clearInterval(intVal);
         } else {
-            pos++;
+            qbObj.position++;
             // qb.style.top = `${pos}px`;
-            qb.style.left = `${pos}px`;
+            qb.style.left = `${qbObj.position}px`;
             qb.style.transform = "rotate(90deg)";
         }
     }
-    const id = setInterval(forward, 15);
-};
+    const intVal = setInterval(forward, 15);
+}
 
 window.onload = () => {
-    const grid = document.getElementById("gamegrid");
-    const qb = { icon: "templates/images/nqb-icon.png", direction: 0, speed: 1 };
+    grid = document.getElementById("gamegrid");
+    qb = document.createElement("div");
+    qb.setAttribute("id", "qb");
+    qbObj.dimension = getDimension();
 
     gridSetup(grid);
-    qbSetup(grid);
+    qbSetup(grid, getDimension());
 }
+
+window.addEventListener("resize", qbResize);
