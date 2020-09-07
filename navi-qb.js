ed5fb27edaf810b2c0fc7ffdfeef33618154f603
LPrivo <privinbox@gmail.com>
2020-09-07 21:12:27 +0100

    repetitive animQb

--- navi-qb.js
+++ navi-qb.js
@@ -1,7 +1,7 @@
 
 let grid;
 let qb = document.getElementById("qb");
-let qbObj = { icon: "url('templates/images/nqb-icon.png')", dimension: 0, position: 0, direction: 0 };
+let qbObj = { icon: "url('templates/images/nqb-icon.png')", dimension: 0, currPos: 0, newPos: 0, orientation: 0 };
 
 const getDimension = () => {
     const dimension = grid.clientHeight / 5;
@@ -35,28 +35,78 @@ const qbResize = () => {
     console.log('qbObj: ', qbObj);
 }
 
-const turn90 = (direction) => {
+const turn90 = (orientation) => {
 
 }
+
+const forward = (intVal) => {
+    if (qbObj.currPos >= qbObj.newPos) {
+        clearInterval(intVal);
+    } else {
+        qbObj.currPos++;
+        qb.style.left = `${qbObj.currPos}px`;
+    }
+}
+
+// const intVal = setInterval(forward, 15);
 
 const animQb = () => {
+    console.log('animQb: clicked');
     // const qb = document.getElementById("qb");
     const dimension = getDimension();
-    // let pos = 0;
-    // let rotateAngle = 90;
-
-    const forward = () => {
-        if (qbObj.position === dimension) {
-            clearInterval(intVal);
-        } else {
-            qbObj.position++;
-            // qb.style.top = `${pos}px`;
-            qb.style.left = `${qbObj.position}px`;
-            qb.style.transform = "rotate(90deg)";
-        }
-    }
+    qbObj.newPos = qbObj.currPos + dimension;
+    console.log('qbObj.newPos: ', qbObj.newPos);
     const intVal = setInterval(forward, 15);
+    forward(intVal);
+    // switch (command) {
+
+    //     case "forward":
+    //         qbObj.newPos = qbObj.currPos + dimension;
+    //         forward();
+    //         break;
+    //     case "backward":
+
+    //         break;
+    //     case "left":
+
+    //         break;
+    //     case "right":
+
+    //         break;
+
+    //     default:
+    //         break;
+    // }
+    // const intVal = setInterval(forward, 15);
+
+    // const forward = () => {
+    //     // console.log('qbObj.currPos: ', qbObj.currPos);
+    //     if (qbObj.currPos >= qbObj.newPos) {
+    //         clearInterval(intVal);
+    //     } else {
+    //         qbObj.currPos++;
+    //         qb.style.left = `${qbObj.currPos}px`;
+    //     }
+    // }
 }
+
+// const animQb = () => {
+//     const qb = document.getElementById("qb");
+//     let pos = 0;
+//     let rotateAngle = 90;
+//     let dimension = 160;
+//     const forward = () => {
+//         if (pos == 160) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             // qb.style.top = `${pos}px`;
+//             qb.style.left = `${pos}px`;
+//             qb.style.transform = "rotate(90deg)";
+//         }
+//     }
+//     const id = setInterval(forward, 15);
+// };
 
 window.onload = () => {
     grid = document.getElementById("gamegrid");
